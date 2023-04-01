c9d550f6ade99295de3f3b849df6977284691a15
Saud Alghamdi <saudalghamdi1997@gmail.com>
2023-04-01 16:57:24 +0300

    Installed memorystore library + improved session storage

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const routes = require("./routes");
 const crypto = require("crypto");
 const session = require("express-session");
+const MemoryStore = require("memorystore")(session);
 
 // BASIC CONFIG
 app.set("view engine", "ejs");
@@ -13,14 +14,31 @@ app.use(express.static(path.join(__dirname, "../client")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// SESSION CONFIG
+// Ensures sessions are securely stored and regularly cleared to prevent any issues with server memory usage //
+
+setInterval(() => {
+  store.clear((err) => {
+    if (err) {
+      console.log("Error clearing expired sessions:", err);
+    }
+  });
+}, 3600000);
+
 app.use(
   session({
     secret: crypto.randomBytes(32).toString("hex"),
     resave: false,
     saveUninitialized: false,
+    store: new MemoryStore({
+      checkPeriod: 3600000,
+    }),
+    cookie: {
+      maxAge: 3600000,
+    },
   })
 );
+
+//---------------------//
 
 app.use(routes);
 
