58e11c82fcd39b28ff9b2b3185eb0efc7acdb267
Saud Alghamdi <saudalghamdi1997@gmail.com>
2023-04-10 07:04:35 +0300

    re-ogranized order

--- server/server.js
+++ server/server.js
@@ -10,23 +10,7 @@ const store = new MemoryStore({
   checkPeriod: 3600000,
 });
 
-// BASIC CONFIG
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../client/views"));
-app.use(express.static(path.join(__dirname, "../client")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Ensures sessions are securely stored and regularly cleared to prevent any issues with server memory usage //
-
-setInterval(() => {
-  store.clear((err) => {
-    if (err) {
-      console.log("Error clearing expired sessions:", err);
-    }
-  });
-}, 3600000);
-
+// Session config + middleware
 app.use(
   session({
     secret: crypto.randomBytes(32).toString("hex"),
@@ -39,8 +23,21 @@ app.use(
   })
 );
 
-//---------------------//
+// Ensures sessions are securely stored and regularly cleared to prevent any issues with server memory usage //
+setInterval(() => {
+  store.clear((err) => {
+    if (err) {
+      console.log("Error clearing expired sessions:", err);
+    }
+  });
+}, 3600000);
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../client/views"));
+
+app.use(express.static(path.join(__dirname, "../client")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => console.log("Listening ..."));
