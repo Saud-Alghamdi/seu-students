1bd0bfa437d09f2e51afc1be4b89999c2dcaf9d4
Saud Alghamdi <saudalghamdi1997@gmail.com>
2023-05-24 15:04:29 +0300

    Removes loader on load

--- client/js/loader.js
+++ client/js/loader.js
@@ -8,6 +8,12 @@ forms.forEach((form) => {
   });
 });
 
+// clear any loader when page has finished loading
+// this is a savior if a user has clicked the back button and a loader just hangs from the previous page
+window.addEventListener("load", () => {
+  removeLoader();
+});
+
 export function removeLoader() {
   const loader = document.getElementById("loader-container");
   loader.classList.add("visually-hidden");
